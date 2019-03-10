ef04fb03fd66ff5348c924a7df3408d7084cb1ac
Gianluca Venturini <gianluca.91@gmail.com>
2019-03-10 12:02:04 -0700

    Web UI bundle config

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return <div>TODO: add app content here</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js',
+  },
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: 'source-map',
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      {test: /\.tsx?$/, loader: 'awesome-typescript-loader'},
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {enforce: 'pre', test: /\.js$/, loader: 'source-map-loader'},
+    ],
+  },
+  // This libraries will be cached by the browser between builds
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+  },
+};
