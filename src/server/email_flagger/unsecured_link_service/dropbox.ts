3aad98ddcf8ca0719bcc56ce17cc66920c9eafcb
Gianluca Venturini <gianluca.91@gmail.com>
2019-03-10 22:55:34 -0700

    Improved accuracy of Dropbox links

--- src/server/email_flagger/unsecured_link_service/dropbox.ts
+++ src/server/email_flagger/unsecured_link_service/dropbox.ts
@@ -3,14 +3,14 @@ import {UnsecuredLinkService} from './types';
 
 export class DropboxService implements UnsecuredLinkService {
   name = 'Dropbox';
-  matchLink = /https:\/\/www.dropbox.com\/[^ \?\"]+/g;
+  matchLink = /https:\/\/www.dropbox.com\/s\/[^ \?\"]+/g;
 
   async isLinkUnsecure(url: string) {
     const matchedPrefix = url.match(/^[^\?]+/g);
     if (matchedPrefix !== null) {
       try {
         // Try to fetch the file. We only verefy that the file exists, we don't download it.
-        const result = await fetch(matchedPrefix + '?dl=1', {size: 0});
+        const result = await fetch(matchedPrefix + '?dl=1', {size: 1});
         if (result.status === 200) {
           return Promise.resolve(true);
         } else {
