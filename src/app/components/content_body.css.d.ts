3f9b1082a2e11cd83b4c4a415e2ce144fe43a750
Gianluca Venturini <gianluca.91@gmail.com>
2019-03-10 18:41:38 -0700

    React virtualized for displaying all the results

--- src/app/components/content_body.css.d.ts
+++ src/app/components/content_body.css.d.ts
@@ -1,2 +1,4 @@
 export const ContentBody: string;
+export const ContentBodyTableHeader: string;
 export const Row: string;
+export const ContentBodyTableBody: string;
--- src/app/components/content_body.tsx
+++ src/app/components/content_body.tsx
@@ -1,5 +1,7 @@
 import * as cx from 'classnames';
 import * as React from 'react';
+import {AutoSizer} from 'react-virtualized/dist/commonjs/AutoSizer';
+import {List, ListRowRenderer} from 'react-virtualized/dist/commonjs/List';
 import {Message} from '../types';
 import * as styles from './content_body.css';
 
@@ -10,36 +12,61 @@ interface ContentBodyProps {
 // TODO: fetch user email from backend in order to redirect correctly in all edge cases
 const userEmail = 0;
 
+/**
+ * Renders all the emails.
+ * It's optimized for rendering large number of elements, it only renders what's visible
+ */
 export const ContentBody = (props: ContentBodyProps) => {
   const makeHandleClick = (emailId: string) => () => {
     window.open(`https://mail.google.com/mail/u/${userEmail}/#sent/${emailId}`);
   };
+  // Function for rendering one row of the table
+  const rowRenderer: ListRowRenderer = ({style, index}) => {
+    const {messages} = props;
+    const message = messages[index];
+    return (
+      <div
+        className={styles.Row}
+        key={`${message.emailServiceId}:${message.emailId}`}
+        onClick={makeHandleClick(message.emailId)}
+        style={style}
+      >
+        <div>{message.from}</div>
+        <div>{message.to}</div>
+        <div>{message.subject}</div>
+        <div>
+          {message.flags.map((flag) => (
+            <span className='bp3-tag .modifier'>{flag}</span>
+          ))}
+        </div>
+        <div>{`${message.date}`}</div>
+      </div>
+    );
+  };
+
   return (
-    <table className={cx('bp3-html-table', '.modifier', styles.ContentBody)}>
-      <thead>
-        <tr>
-          <th>From</th>
-          <th>To</th>
-          <th>Subject</th>
-          <th>Flags</th>
-          <th>Date</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.messages.map((message) => (
-          <tr
-            className={styles.Row}
-            key={`${message.emailServiceId}:${message.emailId}`}
-            onClick={makeHandleClick(message.emailId)}
-          >
-            <td>{message.from}</td>
-            <td>{message.to}</td>
-            <td>{message.subject}</td>
-            <td>{message.flags}</td>
-            <td>{`${message.date}`}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className={styles.ContentBody}>
+      <div className={styles.ContentBodyTableHeader}>
+        <th>From</th>
+        <th>To</th>
+        <th>Subject</th>
+        <th>Flags</th>
+        <th>Date</th>
+      </div>
+      <AutoSizer>
+        {({height, width}) => (
+          <div className={styles.ContentBodyTableBody}>
+            <List
+              height={height}
+              width={width}
+              // Keep this value in sync with css
+              rowHeight={30}
+              rowCount={props.messages.length}
+              rowRenderer={rowRenderer}
+            />
+          </div>
+        )}
+      </AutoSizer>
+    </div>
   );
 };
